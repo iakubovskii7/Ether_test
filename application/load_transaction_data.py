4863e6adc828945142e20438e1186fae0bbd5304
iakubovskii7 <igoryakubovskiy@gmail.com>
2022-04-27 18:08:55 +0300

    add new data

--- application/load_transaction_data.py
+++ application/load_transaction_data.py
@@ -2,9 +2,9 @@ import streamlit as st
 import pandas as pd
 
 
-def load_data():
-    data1 = pd.read_csv("data1.csv")  # from 2022-08-04 to 2021-09-12
-    data2 = pd.read_csv("data2.csv")  # from 2021-09-12 to 2022-04-24
+def load_ether_data():
+    data1 = pd.read_csv("data/transactions_ether/data1.csv")  # from 2022-08-04 to 2021-09-12
+    data2 = pd.read_csv("data/transactions_ether/data2.csv")  # from 2021-09-12 to 2022-04-24
     current_value_col1 = [i for i in data1.columns if i.startswith("CurrentValue @")]
     current_value_col2 = [i for i in data2.columns if i.startswith("CurrentValue @")]
     data1 = data1.drop(columns=current_value_col1)
@@ -23,5 +23,5 @@ def load_data():
 
 
 @st.cache(allow_output_mutation=True, hash_funcs={"_thread.RLock": lambda _: None})
-def load_streamlit_data():
-    return load_data()
+def load_ether_data_st():
+    return load_ether_data()
